968db916a037f865e8f4e7a2432f94f12b4a6baf
brettg17 <brettgelinas7@gmail.com>
2024-08-29 17:04:46 -0600

    completed user log in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in succesfully"
-    flash[:alert] = "Invalid email or password"
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
 
